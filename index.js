2b619e0cae1844ad01033eb2bbad26ab841f2fda
vladsergy <drylb924@gmail.com>
2020-11-17 22:42:32 +0200

    testing external library lodash

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello, Hexlet!');
+import _ from "lodash";
+
+const test = _.intersection([2, 1], [2, 3]);
+console.log(test);
+const test2 = _.capitalize('hello');
+console.log(test2);
